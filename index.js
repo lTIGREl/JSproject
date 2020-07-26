c80c115633b8afd865f13019a394cac46a20e1fc
lTIGREl <68364555+lTIGREl@users.noreply.github.com>
2020-07-26 06:22:14 -0500

    comentarios añadidos

--- index.js
+++ index.js
@@ -2,15 +2,67 @@ window.onload = start;
 
 var formConvalidaciones;
 var refConvalidaciones;
+var refComentarios;
 var tbodyTablaConvalidaciones;
+var detalle;
 
 function start() {
 
-    tbodyTablaConvalidaciones = document.getElementById('cuerpo');
+    tbodyTablaConvalidaciones = document.getElementById('post');
 
     refConvalidaciones = firebase.database().ref().child('Posts');
 
+
     mostrarConvalidacionesDeFirebase();
+}
+function addComments(key1) {
+
+
+    refConvalidaciones.on('value', function (snap) {
+        var data = snap.val();
+
+        var comentarios = data[key1].comments;
+
+        var comentariosAmostrar = "";
+        for (var key in comentarios) {
+            comentariosAmostrar += '<div class="text-content">' +
+                '<div class="row">' +
+
+                '<div class="col-md-1 text-left">' +
+                '<img src="' + comentarios[key].photo + '" alt="Cargando..." style="width:30px;height:30px;">' +
+                '</div>' +
+                '<div class="col-md-11 text-left">' +
+                '<h4>' + comentarios[key].username + '</h4>' +
+
+                '</div>' +
+                '</div>' +
+                '<p>' + comentarios[key].description + '</p>' +
+
+
+                '</div>'
+        }
+
+        detalle.innerHTML += comentariosAmostrar;
+    });
+}
+function mostrarDetalle(foto, imagen, titulo, user, fecha, likes, descripcion, key) {
+    detalle = document.getElementById('first-tab-group');
+    detalles = '<div>' +
+        '<img src="' + imagen + '" alt="Cargando...">' +
+        '<div class="text-content">' +
+        '<h4>' + titulo + '</h4>' +
+        '<ul class="info-post">' +
+        '<li><img src="' + foto + '" alt="Cargando..." style="width:30px;height:30px;"> &nbsp; ' + user + '</li>' +
+        '<li><i class="fa fa-calendar"></i>' + fecha + '</li>' +
+        '<li><i class="fa fa-thumbs-up"></i>' + likes + '</li>' +
+        '</ul>' +
+        '<p>' + descripcion + '</p>' +
+        '</div>' +
+        '</div>';
+    detalle.innerHTML = detalles;
+    addComments(key);
+
+
 }
 
 function mostrarConvalidacionesDeFirebase() {
@@ -18,62 +70,20 @@ function mostrarConvalidacionesDeFirebase() {
         var datos = snap.val();
         var filasAmostrar = "";
         for (var key in datos) {
-            filasAmostrar += '<br><br><br>' +
-                '<div class="row">' +
-                    '<div class="col-lg-8">' +
-                        '<div class="form-group">' +
-                            '<div class="text-right">' +
-                                '<h2 class="nav-link active white">' + datos[key].title + '</h2>' +
-                            '</div>' +
-                        '</div>' +
-                        '<div class="form-group">' +
-                            '<div class="row">' +
-                                '<div class="col-lg-5">' +
-                                    '<div class="text-left">' +
-                                        '<h4>' + datos[key].description +'</h4>' +
-                                    '</div>' +
-                                '</div>' +
-                                '<div class="col-lg-3">' +
-                                    '<img src="' + datos[key].image + '"width="300" alt="Imagen sobre el problema">' +
-                                '</div>' +
-                            '</div>' +
-                        '</div>' +
-                    '</div>' +
-                    '<div class="col-lg-4">' +
-                        '<div class="card">' +
-                            '<div class="card-body">' +
-                                '<div class="form-group">' +
-                                    '<div class="text-left">' +
-                                        '<h5>' + datos[key].username+'</h5>' +
-                                    '</div>' +
-                                    '<img class="text-right"src="' + datos[key].photo + '"width="100">' +
-                                '</div>' +
-                            '</div>' +
-                        '</div>' +
-                    '</div>' +
-                '</div>'
+            var args = "'" + datos[key].photo + "','" + datos[key].image + "','" + datos[key].title + "','" + datos[key].username +
+                "','" + datos[key].date + "','" + datos[key].likes + "','" + datos[key].description + "','" + key + "'";
+            var cm = '"';
+            filasAmostrar += '<li><a class="active">' +
+                datos[key].title +
+                '<ul class="info-post">' +
+                '<li><i class="fa fa-user"> </i>' + datos[key].username + '</li>' +
+                '<li><i class="fa fa-calendar"></i>' + datos[key].date + '</li>' +
+                '<li><i class="fa fa-thumbs-up"></i>' + datos[key].likes + '</li>' +
+                '</ul >' +
+                "<button class='btn btn-primary btn-lg' onclick=" + cm + "return mostrarDetalle(" + args + ");" + cm + ">Detalles</button>" +
+                '</a ></li >'
         }
+
         tbodyTablaConvalidaciones.innerHTML = filasAmostrar;
     });
 }
-
-function enviarConvalidacionAFirebase() {
-    //alert('hola');
-    event.preventDefault();
-    refConvalidaciones.push({
-        cicloAconvalidar: event.target.cicloAconvalidar.value,
-        cicloAportado: event.target.cicloAportado.value,
-        moduloAconvalidar: event.target.moduloAconvalidar.value,
-        moduloAportado: event.target.moduloAportado.value
-    });
-    formConvalidaciones.reset();
-}
-function cargarPublicaciones(titulo, descripcion, imagen, autor, foto) {
-    return '<div class="row"><div class="col-lg-8"><div class="form-group"><div class="text-right"><h2 class="nav-link active white">' +
-        titulo + '</h2></div></div><div class="form-group"><div class="row"><div class="col-lg-5"><div class="text-left"><h4>' +
-        descripcion + '</h4></div></div><div class="col-lg-3"><img src="' + imagen + '"width="400" alt="Imagen sobre el problema"></div></div>' +
-        '</div></div><div class="col-lg-4"><div class="card"><div class="card-body"><div class="form-group"><div class="text-left"><h5>' +
-        autor + '</h5></div><img class="text-right"src="' + foto + '"width="200"></div><div id="editData"></div></div></div></div></div>'
-}
-
-
